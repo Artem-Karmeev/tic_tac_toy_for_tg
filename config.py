cdd60bd3fab389c39fc7e44f13df6b7980b6bc8f
Artem Karmeev <skinnynew161@gmail.com>
2023-02-08 21:34:28 +0300

    hdjvdsjlj

---
+++ config.py
@@ -0,0 +1,5 @@
+from aiogram import Bot, Dispatcher
+
+bot = Bot(token='5936504370:AAG2Gn5BvnN8FYg3xmU62L1z2UAhHPGWELs')
+dp = Dispatcher(bot)
+
---
+++ wolf.py
@@ -0,0 +1,17 @@
+class Quote:
+    
+    my_li = []
+    link = ''
+
+    def __init__(self, path: str = r'quo.txt'):
+        self.link = path
+
+    def open(self):
+        with open(self.link, 'r', encoding='UTF-8') as file:
+            self.my_li = file.readlines()
+
+    def get(self) -> list:
+        return self.my_li
+
+qu = Quote()
+
